a8071222bb73ce3b2b8b910e011b18c6e2ffec49
user <user@pm01-trt.dev231.local>
2018-10-16 13:10:12 -0400

    Example with stage

--- Dockerfile
+++ Dockerfile
@@ -1,4 +1,4 @@
-FROM alpine:latest
+FROM alpine:latest as production
 
 VOLUME ["/mnt"]
 
--- reproduce_error.sh
+++ reproduce_error.sh
@@ -2,9 +2,17 @@
 
 docker-compose up -d
 echo
+echo '###### Notice the container name #####'
+echo
+docker ps -a --format '{{.Names}}' | grep container_name
+echo
 echo '###### Triggering error by triggering an update of the service defining the volume already defined in the docker image ######'
 echo
 docker-compose -f changed-docker-compose.yml up -d
+echo
+echo '###### Notice the container name now has a hash prefix #####'
+echo
+docker ps -a --format '{{.Names}}' | grep container_name
 echo
 echo '###### Cleaning up ######'
 echo
